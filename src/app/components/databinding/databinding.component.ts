1b928910b0efb78ad296eb88a4c777c0a609174e
bhumi47 <bhumika017raut@gmail.com>
2025-02-04 19:31:12 +0530

    data binding

--- src/app/components/databinding/databinding.component.ts
+++ src/app/components/databinding/databinding.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-databinding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './databinding.component.html',
   styleUrl: './databinding.component.css'
 })
 export class DatabindingComponent {
 
+  //string , number, boolean, date
+
+  courseName: string = "Angular 22";
+  stateName: string = "Maharashtra";
+  inputType = "radio";
+  myClassName: string = "bg-primary";
+  rollNo: number = 123;
+  isIndia: boolean = true;
+  currentDate: Date = new Date();
+  
+  firstName = signal("bhumi raut");
+  constructor(){
+   
+  }
+  changeCourseName(){
+     this.courseName = "React Js";
+     this.firstName.set("Kartik Varade");
+  }
+  showAlert(message: string){
+     alert(message)
+  }
 }
