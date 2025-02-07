d5d96df9ba243063d4eacd09686737baadcb1a4a
bhumi47 <bhumika017raut@gmail.com>
2025-02-07 11:35:45 +0530

    control flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { AddEmployeeComponent } from './components/add-employee/add-employee.com
 import { DatabindingComponent } from './components/databinding/databinding.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { StructuralDirComponent } from './directive/structural-dir/structural-dir.component';
+import { IfelseComponent } from './controlFlow/ifelse/ifelse.component';
+import { ForComponent } from './controlFlow/for/for.component';
+import { SwitchComponent } from './controlFlow/switch/switch.component';
 
 export const routes: Routes = [
     {
@@ -21,5 +24,17 @@ export const routes: Routes = [
     {
         path : 'structural-dir',
         component: StructuralDirComponent
+    },
+    {
+        path : 'if-else',
+        component: IfelseComponent
+    },
+    {
+        path : 'for',
+        component : ForComponent
+    },
+    {
+        path: 'switch-case',
+        component: SwitchComponent
     }
 ];
